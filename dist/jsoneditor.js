c57acef98c85918832fc9087b2ba46f1e426a81c
Jichi Guo <jichi@houzz.com>
2015-10-06 13:05:15 -0700

    Add support of trumbowyg for editing html format.

--- dist/jsoneditor.js
+++ dist/jsoneditor.js
@@ -1813,15 +1813,15 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
   },
   setValue: function(value,initial,from_template) {
     var self = this;
-    
+
     if(this.template && !from_template) {
       return;
     }
-    
+
     if(value === null || typeof value === 'undefined') value = "";
     else if(typeof value === "object") value = JSON.stringify(value);
     else if(typeof value !== "string") value = ""+value;
-    
+
     if(value === this.serialized) return;
 
     // Sanitize value before setting it
@@ -1832,9 +1832,12 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
     }
 
     this.input.value = sanitized;
-    
+
     // If using SCEditor, update the WYSIWYG
-    if(this.sceditor_instance) {
+    if(this.trumbowyg_input) {
+      this.trumbowyg_input.trumbowyg('html', sanitized);
+    }
+    else if(this.sceditor_instance) {
       this.sceditor_instance.val(sanitized);
     }
     else if(this.epiceditor) {
@@ -1843,14 +1846,14 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
     else if(this.ace_editor) {
       this.ace_editor.setValue(sanitized);
     }
-    
+
     var changed = from_template || this.getValue() !== value;
-    
+
     this.refreshValue();
-    
+
     if(initial) this.is_dirty = false;
     else if(this.jsoneditor.options.show_errors === "change") this.is_dirty = true;
-    
+
     if(this.adjust_height) this.adjust_height(this.input);
 
     // Bubble this setValue to parents if the value changed
@@ -1859,11 +1862,11 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
   getNumColumns: function() {
     var min = Math.ceil(Math.max(this.getTitle().length,this.schema.maxLength||0,this.schema.minLength||0)/5);
     var num;
-    
+
     if(this.input_type === 'textarea') num = 6;
     else if(['text','email'].indexOf(this.input_type) >= 0) num = 4;
     else num = 2;
-    
+
     return Math.min(12,Math.max(min,num));
   },
   build: function() {
@@ -1958,7 +1961,7 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
       ) {
         this.input_type = this.format;
         this.source_code = true;
-        
+
         this.input = this.theme.getTextareaInput();
       }
       // HTML5 Input type
@@ -1972,7 +1975,7 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
       this.input_type = 'text';
       this.input = this.theme.getFormInputField(this.input_type);
     }
-    
+
     // minLength, maxLength, and pattern
     if(typeof this.schema.maxLength !== "undefined") this.input.setAttribute('maxlength',this.schema.maxLength);
     if(typeof this.schema.pattern !== "undefined") this.input.setAttribute('pattern',this.schema.pattern);
@@ -1991,10 +1994,10 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
     }
 
     this.input
-      .addEventListener('change',function(e) {        
+      .addEventListener('change',function(e) {
         e.preventDefault();
         e.stopPropagation();
-        
+
         // Don't allow changing if this field is a template
         if(self.schema.template) {
           this.value = self.value;
@@ -2002,19 +2005,19 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
         }
 
         var val = this.value;
-        
+
         // sanitize value
         var sanitized = self.sanitize(val);
         if(val !== sanitized) {
           this.value = sanitized;
         }
-        
+
         self.is_dirty = true;
 
         self.refreshValue();
         self.onChange(true);
       });
-      
+
     if(this.options.input_height) this.input.style.height = this.options.input_height;
     if(this.options.expand_height) {
       this.adjust_height = function(el) {
@@ -2041,7 +2044,7 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
           el.style.height = (ch+1)+'px';
         }
       };
-      
+
       this.input.addEventListener('keyup',function(e) {
         self.adjust_height(this);
       });
@@ -2088,12 +2091,27 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
   },
   afterInputReady: function() {
     var self = this, options;
-    
+
     // Code editor
-    if(this.source_code) {      
+    if(this.source_code) {
+      // jichi 10/6/2015: enable trumbowyg when existed
+      if (this.input_type == 'html' &&
+        window.jQuery && window.jQuery.fn && window.jQuery.fn.trumbowyg
+      ) {
+        options = $extend({}, JSONEditor.plugins.trumbowyg, self.options.trumbowyg_options||{});
+        self.trumbowyg_input = window.jQuery(self.input).trumbowyg(options);
+        self.trumbowyg_input.on('tbwchange',function() {
+          var val = this.value;
+          //self.input.value = val; // self.input is the same as this
+          self.value = val;
+          self.is_dirty = true;
+          self.onChange(true);
+        });
+
+      } else
       // WYSIWYG html and bbcode editor
-      if(this.options.wysiwyg && 
-        ['html','bbcode'].indexOf(this.input_type) >= 0 && 
+      if(this.options.wysiwyg &&
+        ['html','bbcode'].indexOf(this.input_type) >= 0 &&
         window.jQuery && window.jQuery.fn && window.jQuery.fn.sceditor
       ) {
         options = $extend({},{
@@ -2102,11 +2120,11 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
           width: '100%',
           height: 300
         },JSONEditor.plugins.sceditor,self.options.sceditor_options||{});
-        
+
         window.jQuery(self.input).sceditor(options);
-        
+
         self.sceditor_instance = window.jQuery(self.input).sceditor('instance');
-        
+
         self.sceditor_instance.blur(function() {
           // Get editor's value
           var val = window.jQuery("<div>"+self.sceditor_instance.val()+"</div>");
@@ -2124,16 +2142,16 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
         this.epiceditor_container = document.createElement('div');
         this.input.parentNode.insertBefore(this.epiceditor_container,this.input);
         this.input.style.display = 'none';
-        
+
         options = $extend({},JSONEditor.plugins.epiceditor,{
           container: this.epiceditor_container,
           clientSideStorage: false
         });
-        
+
         this.epiceditor = new window.EpicEditor(options).load();
-        
+
         this.epiceditor.importFile(null,this.getValue());
-      
+
         this.epiceditor.on('update',function() {
           var val = self.epiceditor.exportFile();
           self.input.value = val;
@@ -2149,7 +2167,7 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
         if(mode === 'cpp' || mode === 'c++' || mode === 'c') {
           mode = 'c_cpp';
         }
-        
+
         this.ace_container = document.createElement('div');
         this.ace_container.style.width = '100%';
         this.ace_container.style.position = 'relative';
@@ -2157,15 +2175,15 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
         this.input.parentNode.insertBefore(this.ace_container,this.input);
         this.input.style.display = 'none';
         this.ace_editor = window.ace.edit(this.ace_container);
-        
+
         this.ace_editor.setValue(this.getValue());
-        
+
         // The theme
         if(JSONEditor.plugins.ace.theme) this.ace_editor.setTheme('ace/theme/'+JSONEditor.plugins.ace.theme);
         // The mode
         mode = window.ace.require("ace/mode/"+mode);
         if(mode) this.ace_editor.getSession().setMode(new mode.Mode());
-        
+
         // Listen for changes
         this.ace_editor.on('change',function() {
           var val = self.ace_editor.getValue();
@@ -2176,7 +2194,7 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
         });
       }
     }
-    
+
     self.theme.afterInputReady(self.input);
   },
   refreshValue: function() {
@@ -2186,7 +2204,10 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
   },
   destroy: function() {
     // If using SCEditor, destroy the editor instance
-    if(this.sceditor_instance) {
+    if (this.trumbowyg_input) {
+      this.trumbowyg_input.trumbowyg('destroy');
+    }
+    else if(this.sceditor_instance) {
       this.sceditor_instance.destroy();
     }
     else if(this.epiceditor) {
@@ -2195,8 +2216,8 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
     else if(this.ace_editor) {
       this.ace_editor.destroy();
     }
-    
-    
+
+
     this.template = null;
     if(this.input && this.input.parentNode) this.input.parentNode.removeChild(this.input);
     if(this.label && this.label.parentNode) this.label.parentNode.removeChild(this.label);
@@ -2213,23 +2234,23 @@ JSONEditor.defaults.editors.string = JSONEditor.AbstractEditor.extend({
   /**
    * Re-calculates the value if needed
    */
-  onWatchedFieldChange: function() {    
+  onWatchedFieldChange: function() {
     var self = this, vars, j;
-    
+
     // If this editor needs to be rendered by a macro template
     if(this.template) {
       vars = this.getWatchedFieldValues();
       this.setValue(this.template(vars),false,true);
     }
-    
+
     this._super();
   },
   showValidationErrors: function(errors) {
     var self = this;
-    
+
     if(this.jsoneditor.options.show_errors === "always") {}
     else if(!this.is_dirty && this.previous_error_setting===this.jsoneditor.options.show_errors) return;
-    
+
     this.previous_error_setting = this.jsoneditor.options.show_errors;
 
     var messages = [];
@@ -7449,17 +7470,17 @@ JSONEditor.defaults.options = {};
 JSONEditor.defaults.translate = function(key, variables) {
   var lang = JSONEditor.defaults.languages[JSONEditor.defaults.language];
   if(!lang) throw "Unknown language "+JSONEditor.defaults.language;
-  
+
   var string = lang[key] || JSONEditor.defaults.languages[JSONEditor.defaults.default_language][key];
-  
+
   if(typeof string === "undefined") throw "Unknown translate string "+key;
-  
+
   if(variables) {
     for(var i=0; i<variables.length; i++) {
       string = string.replace(new RegExp('\\{\\{'+i+'}}','g'),variables[i]);
     }
   }
-  
+
   return string;
 };
 
@@ -7602,11 +7623,14 @@ JSONEditor.plugins = {
   epiceditor: {
 
   },
+  trumbowyg: {
+
+  },
   sceditor: {
 
   },
   select2: {
-    
+
   },
   selectize: {
   }
